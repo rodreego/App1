c67c20ec26e7cf7a151f937cee5feeefb3421b1d
rodreego <rodrigogarcia.vix@gmail.com>
2020-01-29 16:57:05 -0300

    aula 64

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Frase } from '../shared/frase.model'
 import { FRASES } from './frases-mock'
- 
+
 @Component({
   selector: 'app-painel',
   templateUrl: './painel.component.html',
@@ -11,15 +11,40 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES
   public instrucao: string = "Traduza a frase"
-  constructor() {  
-    console.log(this.frases)
+  public resposta: string
+
+  public rodada: number = 0
+  public rodadaFrase: Frase
+
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada]
+    console.log(this.rodadaFrase)
   }
 
   ngOnInit() {
   }
 
-  public atualizaResposta():void {
-    console.log('teste')
+  public atualizaResposta(resposta: Event): void {
+    this.resposta = (<HTMLInputElement>resposta.target).value
+    //console.log(this.resposta)
+  }
+
+  public verificarResposta(): void {
+
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+      alert('A tradução está correta')
+
+      this.rodada++
+      console.log(this.rodada)
+      this.rodadaFrase = this.frases[this.rodada]
+      console.log(this.rodadaFrase)
+
+    } else {
+      alert('A tradução está errada')
+    }
+
+
+
   }
 
 }
